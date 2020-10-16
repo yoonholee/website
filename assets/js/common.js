13a64eaaed0142011d378b3bf0bb4876c8add2c0
Yoonho Lee <yooonholee@gmail.com>
2020-10-16 17:00:18 +0900

    add bib button functionality

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,6 +2,11 @@ $(document).ready(function () {
   // Use abstract button to open and close
   $("a.abstract.publink").click(function () {
     $(this).parent().parent().find(".abstract.hidden").toggleClass("open");
+    $(this).parent().parent().find(".bib.hidden").toggleClass("open", false);
+  });
+  $("a.bib.publink").click(function () {
+    $(this).parent().parent().find(".abstract.hidden").toggleClass("open", false);
+    $(this).parent().parent().find(".bib.hidden").toggleClass("open");
   });
   // Open abstract when targeted (#bibkey in url)
   $("div.row.publication-row:target").find(".abstract.hidden").addClass("open");
